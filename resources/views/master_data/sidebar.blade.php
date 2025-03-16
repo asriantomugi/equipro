84189d5d0c2e83c976cdce6250b820cb6d8b3a26
Mugi Asrianto <asrianto.mugi@gmail.com>
2025-03-17 04:14:49 +0800

    update menu sidebar modul master data

--- resources/views/master_data/sidebar.blade.php
+++ resources/views/master_data/sidebar.blade.php
@@ -19,6 +19,7 @@
 
         <li class="nav-header">NAVIGATION</li>
 
+        <!-- MENU MODULE -->
         <li class="nav-item">
           <a href="{{url('/module')}}" class="nav-link @if($menu == 'Module') active @endif">
             <i class="nav-icon fas fa-th"></i>
@@ -28,6 +29,7 @@
 
         <li class="nav-header">MENU</li>
 
+        <!-- MENU HOME -->
         <li class="nav-item">
           <a href="{{url('/master-data/home')}}" class="nav-link @if($menu == 'Master Data') active @endif">
             <i class="nav-icon fas fa-home"></i>
@@ -35,6 +37,7 @@
           </a>
         </li>
 
+        <!-- MENU USER -->
         <li class="nav-item">
           <a href="{{url('/master-data/user/daftar')}}" class="nav-link @if($menu == 'User') active @endif">
             <i class="nav-icon fas fa-user"></i>
@@ -42,19 +45,55 @@
           </a>
         </li>
 
+        <!-- MENU FASILITAS -->
         <li class="nav-item">
           <a href="{{url('/master-data/fasilitas/daftar')}}" class="nav-link @if($menu == 'Fasilitas') active @endif">
-            <i class="nav-icon fas fa-user"></i>
-            <p>User</p>
+            <i class="nav-icon fas fa-plane"></i>
+            <p>Fasilitas</p>
           </a>
         </li>
 
+        <!-- MENU JENIS ALAT -->
         <li class="nav-item">
           <a href="{{url('/master-data/jenis-alat/daftar')}}" class="nav-link @if($menu == 'JenisAlat') active @endif">
-            <i class="nav-icon fas fa-user"></i>
-            <p>User</p>
+            <i class="nav-icon fas fa-tools"></i>
+            <p>Jenis Alat</p>
           </a>
         </li>
+
+        <!-- MENU LOKASI -->
+        <li class="nav-item @if($menu == 'Lokasi') menu-open @endif">
+          <a href="#" class="nav-link @if($menu == 'Lokasi') active @endif">
+            <i class="nav-icon fas fa-building"></i>
+            <p>Lokasi<i class="right fas fa-angle-left"></i></p>
+          </a>
+
+          <ul class="nav nav-treeview">
+
+            <li class="nav-item">
+              <a href="{{url('/master-data/lokasi-tk-1/daftar')}}" class="nav-link @if($page == 'LokasiTk1') active @endif">
+                <i class="fas fa-caret-right nav-icon"></i>
+                <p>Lokasi Tk. I</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="{{url('/master-data/lokasi-tk-2/daftar')}}" class="nav-link @if($page == 'LokasiTk2') active @endif">
+                <i class="fas fa-caret-right nav-icon"></i>
+                <p>Lokasi Tk. II</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="{{url('/master-data/lokasi-tk-3/daftar')}}" class="nav-link @if($page == 'LokasiTk3') active @endif">
+                <i class="fas fa-caret-right nav-icon"></i>
+                <p>Lokasi Tk. III</p>
+              </a>
+            </li>
+
+          </ul>
+        </li>
+        <!----- END OF MENU USER ----->
 
         <!-- agar menu terakhir tidak tertutup -->
         <li class="nav-item">
